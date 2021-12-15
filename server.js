59fc43be282fbd9ed61213ade801249485410655
Burak Ozdemir <ozdemirburak1912@gmail.com>
2021-12-15 15:22:47 +0300

    routers created

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const questions = require("./routers/question");
+const auth = require("./routers/auth");
 
-const app = express();
 
-const PORT = 5000 || process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello Question Answer Api -- V1");
+//enviroment variables
+
+dotenv.config({
+  path: "./config/env/config.env",
 });
 
+const app = express();
+const PORT = process.env.PORT;
+
+//Routers Middleware
+
+app.use("/api/questions",questions);
+app.use("/api/auth",auth);
+
+//Listen
 app.listen(PORT, () => {
-  console.log(`App Started on ${PORT}`);
+  console.log(`App Started on ${PORT}:${process.env.NODE_ENV}`);
 });
