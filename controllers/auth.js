42382e2879b62e83397c7b809a71651075217ef7
Burak Ozdemir <ozdemirburak1912@gmail.com>
2021-12-15 16:58:18 +0300

    user schema

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,22 @@
-const register = (req, res, next) => {
+const User = require("../models/User");
+
+const register = async (req, res, next) => {
+  //POST DATA
+  const name = "Serhat Say";
+  const email = "serhat@say.com";
+  const password = "123456";
+
+  //async,await
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
   res.status(200).json({
     success: true,
+    data: user,
   });
 };
 
