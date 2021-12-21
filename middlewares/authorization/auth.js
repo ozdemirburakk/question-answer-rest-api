a35c25e966718b7d2418f9dcff23d13fa4eb84ca
Burak Ozdemir <ozdemirburak1912@gmail.com>
2021-12-21 12:28:29 +0300

    admin authentication

--- middlewares/authorization/auth.js
+++ middlewares/authorization/auth.js
@@ -1,5 +1,7 @@
 const CustomError = require("../../helpers/error/CustomError");
 const jwt = require("jsonwebtoken");
+const asyncErrorWrapper = require("express-async-handler");
+const User = require("../../models/User");
 const {
   isTokenIncluded,
   getAccessTokenFromHeader,
@@ -24,14 +26,23 @@ const getAccessToRoute = (req, res, next) => {
       id: decoded.id,
       name: decoded.name,
     };
-  
+
     next();
   });
 };
 
+const getAdminAccess = asyncErrorWrapper(async (req, res, next) => {
+  const { id } = req.user;
 
-const getAdminAccess= 
+  const user = await User.findById(id);
+
+  if (user.role !== "admin") {
+    return next(new CustomError("Only admins can access this route", 403));
+  }
+  next();
+});
 
 module.exports = {
   getAccessToRoute,
+  getAdminAccess,
 };
--- routers/admin.js
+++ routers/admin.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const { getAccessToRoute } = require("../middlewares/authorization/auth");
+const {
+  getAccessToRoute,
+  getAdminAccess,
+} = require("../middlewares/authorization/auth");
 //Block User
 //Delete User
 const router = express.Router();
+router.use([getAccessToRoute, getAdminAccess]);
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    message: "Admin Page",
+  });
+});
 
 module.exports = router;
